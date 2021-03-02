efd43e277a5293ba7d89b61da134c1779b8b11fd
pengcheng.gao <pengcheng.gao@weimob.com>
2021-03-02 10:40:48 +0800

    format

--- app/src/main/java/com/example/androiddevchallenge/ui/page/DetailScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/page/DetailScreen.kt
@@ -33,12 +33,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.androiddevchallenge.MyApp
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.dogsList
-import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.ui.theme.typography
 
 @Composable
